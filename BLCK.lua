8ddf53426a12931ea5cd8e36cda24ba7345db974
ASSISTASD <gty7215@gmail.com>
2024-06-28 15:21:33 +0100

    Update BLCK.lua

--- BLCK.lua
+++ BLCK.lua
@@ -1850,6 +1850,26 @@ local Sectionmisc = MISCTAP:AddSection({
 	Name = "PLAYERS"
 })
 
+MISCTAP:AddButton({
+	Name = "TELEPORT TOOL",
+	Callback = function()
+local plr = game:GetService("Players").LocalPlayer
+local mouse = plr:GetMouse()
+local tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "Teleport Tool"
+tool.Activated:Connect(function()
+local root = plr.Character.HumanoidRootPart
+local pos = mouse.Hit.Position+Vector3.new(0,2.5,0)
+local offset = pos-root.Position
+root.CFrame = root.CFrame+offset
+end)
+tool.Parent = plr.Backpack
+    end
+})
+
+
+
 MISCTAP:AddToggle({
 	Name = "NO STUN",
 	Default = false,
